b1c290734de19c85c8919379f5afbd8fdc109feb
Daisuke <mhdf23888@gmail.com>
2024-04-17 11:00:48 +0800

    updating py-astro-aleph/patches/patch-aleph_AstEph.py on 2024-04-17T03:00:47.960359+00:00

---
+++ py-astro-aleph/patches/patch-aleph_AstEph.py
@@ -0,0 +1,20 @@
+$NetBSD$
+
+--- aleph/AstEph.py.orig	2024-04-17 02:48:33.067972231 +0000
++++ aleph/AstEph.py
+@@ -6,7 +6,7 @@ from astropy.time import Time
+ import sqlite3, time, warnings, os
+ import numpy as np
+ import multiprocessing as mp
+-from astropy.coordinates.angle_utilities import angular_separation
++from astropy.coordinates import angular_separation
+ from .aleph_utils import *
+ from astropy.table import QTable, Table, hstack
+ 
+@@ -54,4 +54,4 @@ class AstEph:
+             info = np.array(self.cursor.fetchall(), dtype=str)
+             self.numbers_db = np.array([getint(num) for num in info[:,1]])
+             self.names_db = np.array([nm.strip() for nm in info[:,0]])
+-            self.H_db = info[:,2].astype(float); self.G_db = info[:,3].astype(float)
+\ No newline at end of file
++            self.H_db = info[:,2].astype(float); self.G_db = info[:,3].astype(float)
