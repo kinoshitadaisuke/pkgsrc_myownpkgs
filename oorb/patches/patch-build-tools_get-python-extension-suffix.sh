398f1da12340a3dd52a9885127a4852ef54b3152
Daisuke <mhdf23888@gmail.com>
2026-08-26 08:43:16 +0800

    adding "patches/patch-build-tools_get-python-extension-suffix.sh" on 26/Aug/2026

---
+++ oorb/patches/patch-build-tools_get-python-extension-suffix.sh
@@ -0,0 +1,10 @@
+$NetBSD$
+
+--- build-tools/get-python-extension-suffix.sh.orig	2026-08-25 15:44:21.289781794 +0000
++++ build-tools/get-python-extension-suffix.sh
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/usr/pkg/bin/bash
+ 
+ PYTHON=${PYTHON:-python}
+ 
