e6e83b159a209d3f12310e892b730a7932f11199
Daisuke <mhdf23888@gmail.com>
2026-08-26 08:43:09 +0800

    adding "patches/patch-build-tools_generate-version-pyf.sh" on 26/Aug/2026

---
+++ oorb/patches/patch-build-tools_generate-version-pyf.sh
@@ -0,0 +1,10 @@
+$NetBSD$
+
+--- build-tools/generate-version-pyf.sh.orig	2026-08-25 15:44:06.040189212 +0000
++++ build-tools/generate-version-pyf.sh
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/usr/pkg/bin/bash
+ #
+ # (Re)Generate contents of version.pyf with the version given on the command
+ # line. Output to stdout. Called from build/Makefile.
