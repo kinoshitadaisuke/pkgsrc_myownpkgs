1bda9fb7d3da56a6b9d36614629dc6f886a812fb
Daisuke <mhdf23888@gmail.com>
2026-08-26 08:42:46 +0800

    adding "patches/patch-build-tools_compute-version.sh" on 26/Aug/2026

---
+++ oorb/patches/patch-build-tools_compute-version.sh
@@ -0,0 +1,10 @@
+$NetBSD$
+
+--- build-tools/compute-version.sh.orig	2026-08-25 15:43:26.549264005 +0000
++++ build-tools/compute-version.sh
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/usr/pkg/bin/bash
+ #
+ # Create PEP-440 compliant version identifiers using information stored in
+ # git.
