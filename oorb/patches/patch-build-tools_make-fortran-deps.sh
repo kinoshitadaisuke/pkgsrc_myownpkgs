af75137ac1745a31c843183f9623c33b5ad04415
Daisuke <mhdf23888@gmail.com>
2026-08-26 08:43:24 +0800

    adding "patches/patch-build-tools_make-fortran-deps.sh" on 26/Aug/2026

---
+++ oorb/patches/patch-build-tools_make-fortran-deps.sh
@@ -0,0 +1,10 @@
+$NetBSD$
+
+--- build-tools/make-fortran-deps.sh.orig	2026-08-25 15:44:32.330099691 +0000
++++ build-tools/make-fortran-deps.sh
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/usr/pkg/bin/bash
+ #
+ #  Inspired by the solution by FX Coudert
+ #       https://gcc.gnu.org/ml/fortran/2007-11/msg00013.html
